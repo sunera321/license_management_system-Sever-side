2e16b1222e247441da9f1c891c3a60aebb7a66f2
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-02-20 22:08:31 +0530

    add email genarator api

--- license_management_system Sever side/Program.cs
+++ license_management_system Sever side/Program.cs
@@ -1,13 +1,19 @@
+using Email_Test.EmailService;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add services to the container.
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
