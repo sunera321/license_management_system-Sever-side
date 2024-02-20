cd21aa1e771859b0b4496eb66c61087dcae12d32
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-02-20 21:55:13 +0530

    test

---
+++ license_management_system Sever side/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace license_management_system_Sever_side.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
