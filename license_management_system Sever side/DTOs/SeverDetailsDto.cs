2eb24b38ecc2f23303eb44aa475d8d40ea03f286
19840769 <kodikarah.21@uom.lk>
2024-03-31 08:33:51 +0530

    Merge

---
+++ license_management_system Sever side/DTOs/SeverDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace license_management_system_Sever_side.DTOs
+{
+    public struct SeverDetailsDto
+    {
+        public string HostUrl { get; set; }
+        public string MacAddress { get; set; }
+        public string[] SiteNames { get; set; }
+    }
+}
