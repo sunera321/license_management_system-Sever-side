a9d1af34aeb6606fd9a34718eb869b682457c5c6
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-04-03 20:50:11 +0530

    add email sercicers and Dtos

---
+++ license_management_system Sever side/Models/DTOs/SendClintMailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace license_management_system_Sever_side.Models.DTOs
+{
+    public class SendClintMailDto
+    {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ContactInfo { get; set; }
+
+    }
+}
