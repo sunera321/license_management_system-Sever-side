8cbd2a6db0a36305d1649677a096ff49a4d54dd2
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-04-03 10:16:24 +0530

    crete relationship

--- license_management_system Sever side/Models/Entities/RequestKey.cs
+++ license_management_system Sever side/Models/Entities/RequestKey.cs
@@ -10,14 +10,14 @@ namespace license_management_system_Sever_side.Models.Entities
         [Key, Column("request_id")]
         [DisplayName("Request ID")]
         public int RequestID { get; set; }
-/*
-        [Column("status_Partner_mgt")]
+
+        [Column("status_finance_mgt")]
         [DisplayName("Finance Manager Status")]
         public RequestStatus? isFinanceApproval { get; set; } = RequestStatus.Pending;
 
-        [Column("comment_finace_mgt")]
+        [Column("status_Partner_mgt")]
         [DisplayName("Partner Manager Comment")]
-        public RequestStatus? isPartnerApproval { get; set; } = RequestStatus.Pending;*/
+        public RequestStatus? isPartnerApproval { get; set; } = RequestStatus.Pending;
 
         [Column("comment_finace_mgt"), MaxLength(50)]
         [DisplayName("Finace Manager Comment")]
@@ -29,11 +29,11 @@ namespace license_management_system_Sever_side.Models.Entities
         public int NumberOfDays { get; set; }
 
 
-
+        [ForeignKey("EndClientId")]
         public int EndClientId { get; set; }
 
         // Navigation property back to EndClient
-        [ForeignKey("EndClientId")]
+
         public virtual EndClient EndClient { get; set; }
 
 
@@ -41,29 +41,29 @@ namespace license_management_system_Sever_side.Models.Entities
         // Navigation property for the related Modules
         public virtual ICollection<Modules> Modules { get; set; }
 
-
+        [ForeignKey("LicenseKeyId")]
         public int LicenseKeyId { get; set; }
         // Navigation property back to LicenseKey
-        [ForeignKey("LicenseKeyId")]
+        
         public virtual LicenseKey LicenseKey { get; set; }
 
-
+        [ForeignKey("PartnerId ")]
         public int PartnerId { get; set; }
         // Navigation property back to LicenseKey
-        [ForeignKey("PartnerId ")]
+       
         public virtual Partner Partner { get; set; }
 
 
-
+        [ForeignKey("FinaceManagerId")]
         public int FinaceManagerId { get; set; }
         // Navigation property back to LicenseKey
-        [ForeignKey("FinaceManagerId")]
+       
         public virtual FinaceManager FinaceManager { get; set; }
 
-
+        [ForeignKey("PartnerManagerID")]
         public int PartnerManagerID { get; set; }
         // Navigation property back to LicenseKey
-        [ForeignKey("PartnerManagerID")]
+        
         public virtual PartnerManager PartnerManager { get; set; }
     }
 
--- license_management_system Sever side/Program.cs
+++ license_management_system Sever side/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllers();*/
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDbCon"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
