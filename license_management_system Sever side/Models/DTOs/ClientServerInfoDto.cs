30a86016646d26e2844db2e5b3a354adcf48c1ff
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-04-10 01:43:31 +0530

    add clint sever info methord and controller

---
+++ license_management_system Sever side/Models/DTOs/ClientServerInfoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace license_management_system_Sever_side.Models.DTOs
+{
+    public class ClientServerInfoDto
+    {
+        public string HostUrl { get; set; }
+        public string MacAddress { get; set; }
+        public string[] SiteNames { get; set; }
+    }
+}
