0b67f274890ce7b2e9e0d469b2ea22ddc42e4e96
19840769 <kodikarah.21@uom.lk>
2024-04-09 22:49:58 +0530

    updated

---
+++ license_management_system Sever side/Models/DTOs/LicenseKeyDto.cs
@@ -0,0 +1,6 @@
+﻿namespace license_management_system_Sever_side.Models.DTOs
+{
+    public class LicenseKeyDto
+    {
+    }
+}
--- license_management_system Sever side/Models/Entities/RequestKey.cs
+++ license_management_system Sever side/Models/Entities/RequestKey.cs
@@ -2,21 +2,26 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
 using Foolproof;
+using System.Text.Json.Serialization;
 
 namespace license_management_system_Sever_side.Models.Entities
 {
     public class RequestKey
     {
+     
+
         [Key, Column("request_id")]
         [DisplayName("Request ID")]
         public int RequestID { get; set; }
 
         [Column("status_finance_mgt")]
         [DisplayName("Finance Manager Status")]
+        [EnumDataType(typeof(RequestStatus), ErrorMessage = "Invalid status. Please select from Pending, Approved, or Rejected.")]
         public RequestStatus? isFinanceApproval { get; set; } = RequestStatus.Pending;
 
         [Column("status_Partner_mgt")]
         [DisplayName("Partner Manager Comment")]
+        [EnumDataType(typeof(RequestStatus), ErrorMessage = "Invalid status. Please select from Pending, Approved, or Rejected.")]
         public RequestStatus? isPartnerApproval { get; set; } = RequestStatus.Pending;
 
         [Column("comment_finace_mgt"), MaxLength(50)]
@@ -39,14 +44,14 @@ namespace license_management_system_Sever_side.Models.Entities
 
 
 
-       
-
+        /*
+        [JsonIgnore]
         [ForeignKey("LicenseKeyId")]
         public int LicenseKeyId { get; set; }
         // Navigation property back to LicenseKey
-        
+        [JsonIgnore]
         public virtual LicenseKey LicenseKey { get; set; }
-
+        */
         [ForeignKey("PartnerId ")]
         public int PartnerId { get; set; }
         // Navigation property back to LicenseKey
