ea060c48642556dc878ea87592d7080717945ca2
19840769 <kodikarah.21@uom.lk>
2024-04-13 22:05:38 +0530

    up

--- license_management_system Sever side/Models/Entities/RequestKey.cs
+++ license_management_system Sever side/Models/Entities/RequestKey.cs
@@ -9,6 +9,12 @@ namespace license_management_system_Sever_side.Models.Entities
 {
     public class RequestKey
     {
+        public RequestKey()
+        {
+            isFinanceApproval = false;
+            isPartnerApproval = false;
+        }
+
         [Key, Column("request_id")]
         [DisplayName("Request ID")]
         public int RequestID { get; set; }
@@ -17,55 +23,57 @@ namespace license_management_system_Sever_side.Models.Entities
         [DisplayName("Finance Manager Status")]
 
         
-        public Boolean isFinanceApproval { get; set; } = false;
-
+        public bool isFinanceApproval { get; set; } 
+        
         [Column("status_Partner_mgt")]
         [DisplayName("Partner Manager Status")]
         
-        public Boolean isPartnerApproval { get; set; } =false;
-
+        public bool isPartnerApproval { get; set; }
 
 
         [Column("comment_finace_mgt"), MaxLength(50)]
         [DisplayName("Finace Manager Comment")]
-        public string? CommentFinaceMgt { get; set; } =string.Empty;
-
+        public string? CommentFinaceMgt { get; set; } = null;
 
         [Column("comment_partner_mgt"), MaxLength(50)]
         [DisplayName("Partner Manager Comment")]
-        public string? CommentPartnerMgt { get; set; } = string.Empty;
+        public string? CommentPartnerMgt { get; set; } = null;
         public int NumberOfDays { get; set; }
 
 
 
         [ForeignKey("EndClientId")]
         public int EndClientId { get; set; }
+
+        // Navigation property back to EndClient
+
         public virtual EndClient EndClient { get; set; }
 
-
         [ForeignKey("PartnerId ")]
-        public int? PartnerId { get; set; }
-        public virtual Partner? Partner { get; set; }
+        public int PartnerId { get; set; }
+        // Navigation property back to LicenseKey
+       
+        public virtual Partner Partner { get; set; }
 
 
         [ForeignKey("FinaceManagerId")]
         public int? FinaceManagerId { get; set; }
-        public virtual FinaceManager? FinaceManager { get; set; }
-
+        // Navigation property back to LicenseKey
+       
+        public virtual FinaceManager FinaceManager { get; set; }
 
         [ForeignKey("PartnerManagerID")]
         public int? PartnerManagerID { get; set; }
-        public virtual PartnerManager? PartnerManager { get; set; }
-
-
-        public ICollection<Modules> Modules { get; set; }
-
-        [JsonIgnore]
-        public License_key? License_key { get; set; }
-
+        // Navigation property back to LicenseKey
         
+        public virtual PartnerManager? PartnerManager { get; set; }
+        [JsonIgnore]
+        public ICollection<Modules> Modules { get; set; }
+        [JsonIgnore]
+        public LicenseKey? LicenseKey { get; set; }
 
     }
+
 
     public enum RequestStatus
     {
