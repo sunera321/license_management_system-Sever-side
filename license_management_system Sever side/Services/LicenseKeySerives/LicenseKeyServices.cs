31db3c634edaecdace445b804802b67f6b62114c
19840769 <kodikarah.21@uom.lk>
2024-04-22 14:45:10 +0530

    update

--- license_management_system Sever side/Services/LicenseKeySerives/LicenseKeyServices.cs
+++ license_management_system Sever side/Services/LicenseKeySerives/LicenseKeyServices.cs
@@ -5,6 +5,7 @@ using license_management_system_Sever_side.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Text;
 
 namespace license_management_system_Sever_side.Services.LicenseKeyServices
 {
@@ -12,7 +13,7 @@ namespace license_management_system_Sever_side.Services.LicenseKeyServices
     {
         private readonly DataContext _context;
         private readonly IMapper _mapper;
-
+ 
         public LicenseKeyServices(DataContext context, IMapper mapper)
         {
             _context = context;
@@ -42,6 +43,97 @@ namespace license_management_system_Sever_side.Services.LicenseKeyServices
             _context.License_keys.Remove(licenseKey);
             await _context.SaveChangesAsync();
         }
+        public async Task<string> GenerateLicenseKey(int endClientId, int requestKeyId)
+        {
+            try
+            {
+                if (endClientId <= 0 || requestKeyId <= 0)
+                {
+                    throw new ArgumentException("Invalid client or request key ID");
+                }
 
+                var endClient = await _context.EndClients.FirstOrDefaultAsync(e => e.Id == endClientId);
+                var requestKey = await _context.RequestKeys.FirstOrDefaultAsync(r => r.RequestID == requestKeyId);
+
+                if (endClient != null && requestKey != null)
+                {
+                    string email = endClient.Email;
+                    string macAddress = endClient.MackAddress;
+                    string hostUrl = endClient.HostUrl;
+
+                    string combinedData = email + macAddress + hostUrl;
+                    string hashedKey = HashString(combinedData);
+
+                    var license = new License_key
+                    {
+                        Key_name = hashedKey,
+                        ActivationDate = DateTime.Now,
+                        DeactivatedDate = DateTime.Now.AddDays(requestKey.NumberOfDays),
+                        Key_Status = "Activated", // Assuming you want to activate the key upon generation
+                        RequestId = requestKey.RequestID,
+                        ClintId = endClient.Id
+                    };
+
+                    _context.License_keys.Add(license);
+                    await _context.SaveChangesAsync();
+
+                    return hashedKey;
+                }
+                else
+                {
+                    throw new KeyNotFoundException("End client or request key not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error generating license key", ex);
+            }
+        }
+
+        public async Task<string> DecodeLicenseKeyByRequestId(int requestId)
+        {
+            try
+            {
+                var licenseKey = await _context.License_keys.FirstOrDefaultAsync(l => l.RequestId == requestId);
+
+                if (licenseKey != null)
+                {
+                    string decodedKey = DecodeString(licenseKey.Key_name);
+                    return decodedKey;
+                }
+                else
+                {
+                    throw new KeyNotFoundException("License key not found for the given request ID.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error decoding license key", ex);
+            }
+        }
+
+        private string HashString(string input)
+        {
+            try
+            {
+                return Convert.ToBase64String(Encoding.UTF8.GetBytes(input));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error hashing string", ex);
+            }
+        }
+
+        private string DecodeString(string input)
+        {
+            try
+            {
+                return Encoding.UTF8.GetString(Convert.FromBase64String(input));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error decoding string", ex);
+            }
+        }
     }
 }
